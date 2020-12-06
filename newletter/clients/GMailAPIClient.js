37633f1ec14a8570fcf0d6885904d2a0e7d28ebe
ptroche <trochepablodaniel@gmail.com>
2020-12-06 20:22:15 -0300

    fix path credenciales gmail

--- newletter/clients/GMailAPIClient.js
+++ newletter/clients/GMailAPIClient.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const path = require('path')
 const {google} = require('googleapis');
 
 
 class GMailAPIClient {
 
-    constructor(credentials_path='./credentials.json', token_path='./token.json') {
+    constructor(credentials_path=path.join(__dirname, 'credentials.json'), token_path=path.join(__dirname, 'token.json')) {
         if (!fs.existsSync(credentials_path)) {
             throw new Error(`Credentials file not found: ${credentials_path}`);
         }
